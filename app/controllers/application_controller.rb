1ba81780e943a48556f328db0f8a0a34f1a5169d
Beth Ruff <beth.ruff2@gmail.com>
2020-04-23 21:21:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do 
     @article = Article.find(params[:id])
-    @article.update(params[:article]).save
+    @article.update.save(params[:article])
     redirect to "/articles/#{@article.id}"
   end 
   
