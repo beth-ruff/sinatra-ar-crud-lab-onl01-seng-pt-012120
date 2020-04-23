e2a61192a24a6bc833ffe310eaba8f7e7de19997
Beth Ruff <beth.ruff2@gmail.com>
2020-04-23 20:49:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end 
   
   get '/articles/:id' do 
-    @article = Article.find(params[:id])
+    @article = Article.find_by_id(params[:id])
     erb :show 
     # binding.pry
   end 
